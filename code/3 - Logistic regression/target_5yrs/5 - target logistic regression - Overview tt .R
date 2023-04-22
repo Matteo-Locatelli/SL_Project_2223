7f3ec5b6c26f3711a4b7244722b0a23feffe25aa
Matteo-Locatelli <m.locatelli79@studenti.unibg.it>
2023-04-22 09:54:07 +0200

    Update LogReg with threshold

--- code/3 - Logistic regression/target_5yrs/5 - target logistic regression - Overview tt .R
+++ code/3 - Logistic regression/target_5yrs/5 - target logistic regression - Overview tt .R
@@ -31,7 +31,7 @@ train <- sample(1:nrow(NbaPlayers), floor(nrow(NbaPlayers)*0.75))
 ### Logistic regression with all predictors
 
 log_reg_all <- glm( target ~ .-target_5yrs,
-                data = NbaPlayers , subset = train, family = binomial)
+                    data = NbaPlayers , subset = train, family = binomial)
 
 # Positive/Negative coefficient for a predictor
 summary(log_reg_all)
@@ -39,14 +39,14 @@ summary(log_reg_all)
 coef(log_reg_all)
 
 # Function to predict the probability of a rookie
-fit_all <- predict( log_reg_all , type = "response",  newx = NbaPlayers[-train, c(-18,-19)])
+fit_all <- predict( log_reg_all, NbaPlayers[-train,], type = "response")
 
 # Boolean vector of 1250 values
-clas_all <- rep ( " No ", dim(NbaPlayers)[1])
+clas_all <- rep ( " No ", dim(NbaPlayers)[1]-length(train))
 clas_all[fit_all > .5] = " Yes ";
 
 # Show confusion matrix
-table_all <- table(clas_all, NbaPlayers$target)
+table_all <- table(clas_all, NbaPlayers$target[-train])
 table_all
 
 # Evaluate the error rate
@@ -54,7 +54,7 @@ err_rate_all = (table_all[1,2] + table_all[2,1]) / sum(table_all)
 err_rate_all
 
 # Percentage of correct predictions
-mean(clas_all == NbaPlayers$target)
+mean(clas_all == NbaPlayers$target[-train])
 
 
 ### Logistic regression with only important predictors
@@ -68,14 +68,14 @@ summary(log_reg_imp)
 coef(log_reg_imp)
 
 # Function to predict the probability of a rookie
-fit_imp <- predict( log_reg_imp , type = "response", newx = NbaPlayers[-train, c(-18,-19)])
+fit_imp <- predict( log_reg_imp, NbaPlayers[-train,], type = "response")
 
 # Boolean vector of 1250 values
-clas_imp <- rep ( " No ", dim(NbaPlayers)[1])
+clas_imp <- rep ( " No ", dim(NbaPlayers)[1]-length(train))
 clas_imp[fit_imp > .5] = " Yes ";
 
 # Show confusion matrix
-table_imp <- table(clas_imp, NbaPlayers$target)
+table_imp <- table(clas_imp, NbaPlayers$target[-train])
 table_imp
 
 # Evaluate the error rate
@@ -83,7 +83,7 @@ err_rate_imp = (table_imp[1,2] + table_imp[2,1]) / sum(table_imp)
 err_rate_imp
 
 # Percentage of correct predictions
-mean(clas_imp == NbaPlayers$target)
+mean(clas_imp == NbaPlayers$target[-train])
 
 # Slightly worse than before
 
@@ -95,12 +95,12 @@ p = double(9)
 
 for(ptry in 1:9){
   log_reg <- glm( target ~ gp + oreb,
-                  data = NbaPlayers , subset = train, family = binomial )
-  fit <- predict( log_reg , type = "response",  newx = NbaPlayers[-train, c(-18,-19)])
-  clas <- rep ( " No " , dim(NbaPlayers)[1])
+                  data = NbaPlayers, subset = train, family = binomial )
+  fit <- predict( log_reg , NbaPlayers[-train,], type = "response")
+  clas <- rep ( " No " , dim(NbaPlayers)[1]-length(train))
   p[ptry] = ptry*0.1
   clas[fit > p[ptry]] = " Yes ";
-  table <- table(clas, NbaPlayers$target)
+  table <- table(clas, NbaPlayers$target[-train])
   test_err_rate[ptry] = (table[1,2] + table[2,1]) / sum(table)
 }
 
@@ -108,35 +108,30 @@ plot(p, test_err_rate, type='b', main="Logistic Regression Test Error(p)",
      ylab="test err rate", xlab="p", col='blue', pch=18,
      lwd=3, xaxp = c(0, 0.9, 9))
 
-which.min(test_err_rate) # 5
+which.min(test_err_rate) # 3
 min(test_err_rate)
 
-# Try 30 values from 0 to 0.3 for optimal threshold
-# Not less because there are no "No" in the prediction
-# ptry <= 8 gives only "Yes" in the prediction 
+# Try 30 values from 0.2 to 0.5 for optimal threshold
 
 test_err_rate2 = double(30)
 p2 = double(30)
 
 for(ptry in 1:30){
   log_reg2 <- glm( target ~ gp + oreb,
-                  data = NbaPlayers , subset = train, family = binomial )
-  fit2 <- predict( log_reg2 , type = "response", newx = NbaPlayers[-train, c(-18,-19)])
-  clas2 <- rep ( " No " , dim(NbaPlayers)[1])
-  p2[ptry] = 0 + ptry*0.01
+                   data = NbaPlayers , subset = train, family = binomial )
+  fit2 <- predict( log_reg2, NbaPlayers[-train,], type = "response")
+  clas2 <- rep ( " No " , dim(NbaPlayers)[1]-length(train))
+  p2[ptry] = 0.2 + ptry*0.01
   clas2[fit2 > p2[ptry]] = " Yes ";
-  table2 <- table(clas2, NbaPlayers$target)
-  if (ptry < 9)
-    test_err_rate2[ptry] = table2[1,1] / sum(table2)
-  else 
-    test_err_rate2[ptry] = (table2[1,2] + table2[2,1]) / sum(table2)
+  table2 <- table(clas2, NbaPlayers$target[-train])
+  test_err_rate2[ptry] = (table2[1,2] + table2[2,1]) / sum(table2)
 }
 
 plot(p2, test_err_rate2, type='b', main="Logistic Regression Test Error(p)",
      ylab="test err rate", xlab="p", col='blue', pch=18,
-     lwd=3, xaxp = c(0, 0.3, 30))
+     lwd=3, xaxp = c(0.2, 0.5, 30))
 
-which.min(test_err_rate2) # minimum for p = 0.24
+which.min(test_err_rate2) # minimum for p = 0.36
 min(test_err_rate2)
 
 
@@ -151,14 +146,14 @@ summary(log_reg_opt)
 coef(log_reg_opt)
 
 # Function to predict the probability of a rookie
-fit_opt <- predict( log_reg_opt , type = "response", newx = NbaPlayers[-train, c(-18,-19)])
+fit_opt <- predict( log_reg_opt, NbaPlayers[-train,], type = "response")
 
 # Boolean vector of 1250 values
-clas_opt <- rep ( " No ", dim(NbaPlayers)[1])
-clas_opt[fit_opt > .24] = " Yes ";
+clas_opt <- rep ( " No ", dim(NbaPlayers)[1]-length(train))
+clas_opt[fit_opt > .36] = " Yes ";
 
 # Show confusion matrix
-table_opt <- table(clas_opt, NbaPlayers$target)
+table_opt <- table(clas_opt, NbaPlayers$target[-train])
 table_opt
 
 # Evaluate the error rate
@@ -166,4 +161,4 @@ err_rate_opt = (table_opt[1,2] + table_opt[2,1]) / sum(table_opt)
 err_rate_opt
 
 # Percentage of correct predictions
-mean(clas_opt == NbaPlayers$target)
+mean(clas_opt == NbaPlayers$target[-train])
